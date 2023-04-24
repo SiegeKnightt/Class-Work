c36b7e85500ff843b1001a7f81002dc3ed35dc8a
Reeves Farrell <93102625+SiegeKnightt@users.noreply.github.com>
2023-04-24 14:26:46 -0400

    Add files via upload

---
+++ CPSC 2311/Lab 11/driver.c
@@ -0,0 +1,20 @@
+/**************************
+*Reeves Farrell
+*CPSC 2310 Spring 23
+*UserName: jrfarre@clemson.edu
+*Instructor:  Dr. Yvon Feaster
+*************************/
+
+#include <stdio.h>
+
+int main(){
+
+		int min = 0;
+	int max = 10;
+		for(int i = 0; i <= max; i++){
+			printf("%d ", i);
+		}
+
+	printf("\n");
+	return 0;
+}
